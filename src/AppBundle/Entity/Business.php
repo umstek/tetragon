845c624c64a2885edfb4f57089a22c7ee21adf74
Akila D. Perera <akilamatrix@gmail.com>
2016-04-07 18:15:03 +0000

    Attributes

--- src/AppBundle/Entity/Business.php
+++ src/AppBundle/Entity/Business.php
@@ -6,74 +6,18 @@ class Business
 {
     private $manager;
 
-    private $date;
+    private $seller = array ();
 
-    private $items;
+    private $customer = array ();
 
-    private $customer;
+    private $repairingItem = array ();
 
-    private $seller;
+    private $sellerItems = array ();
+    
+    private $technician = array ();
 
     public function __construct()
     {
-        $this->items = new ArrayCollection();
     }
 
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    public function setDate($date)
-    {
-        $this->date = $date;
-
-        return $this;
-    }
-
-    public function getDate()
-    {
-        return $this->date;
-    }
-
-    public function addItem(\AppBundle\Entity\SellingItem $item)
-    {
-        $this->items[] = $item;
-
-        return $this;
-    }
-
-    public function removeItem(\AppBundle\Entity\SellingItem $item)
-    {
-        $this->items->removeElement($item);
-    }
-
-    public function getItems()
-    {
-        return $this->items;
-    }
-
-    public function setCustomer(\AppBundle\Entity\Customer $customer = null)
-    {
-        $this->customer = $customer;
-
-        return $this;
-    }
-
-    public function getCustomer()
-    {
-        return $this->customer;
-    }
-
-    public function setSeller(\AppBundle\Entity\Seller $seller = null)
-    {
-        $this->seller = $seller;
-
-        return $this;
-    }
-
-    public function getSeller()
-    {
-        return $this->seller;
-    }
 }
