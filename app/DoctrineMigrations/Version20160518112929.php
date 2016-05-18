f2dc75e90a21c1ae6ee9b9d186b98a2fd4b143e9
Wickramaranga <umstek@live.com>
2016-05-18 15:09:50 +0530

    Fix. Run migrations.

---
+++ app/DoctrineMigrations/Version20160518112929.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160518112929 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE selling_item CHANGE warranty_expiration warranty_expiration DATETIME DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE selling_item CHANGE warranty_expiration warranty_expiration DATETIME NOT NULL');
+    }
+}
